900c5ec3111bf30efb884b8a23cdec54df67d14a
Markuson <el.marcuson@gmail.com>
2020-10-07 11:47:46 +0200

    working on responsiveness

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -2,44 +2,46 @@ import styles from '../../styles/styles.module.css'
 
 export default function Navbar() {
 
-    const textStyles = () => {
+    const navbarStyles = () => {
         return {
-            fontSize: "1.2rem"
+            zIndex: "1000"
         }
     }
 
-    return <div data-uk-sticky="sel-target: .uk-navbar-container;  top: #navbar; animation: uk-animation-slide-top; bottom: #sticky-on-scroll-up; cls-inactive: uk-navbar-transparent; cls-active: navbar">
-        <nav id="navbar" className="uk-width-1-1 uk-navbar-container uk-dark uk-position-top" data-uk-navbar data-uk-scrollspy="cls:uk-animation-fade ">
-            <div className="uk-navbar-left uk-padding-small">
-                <div className="uk-padding-large uk-padding-remove-vertical">
-                    <img className="logo" src='logo.png' alt="soulmountain" width="200px" />
+    return <div className="uk-visible@m">
+        <div data-uk-sticky="sel-target: .uk-navbar-container;  top: .uk-navbar-container; animation: uk-animation-slide-top; bottom: #sticky-on-scroll-up; cls-inactive: uk-navbar-transparent; cls-active: navbar">
+            <nav id="navbar" className="uk-width-1-1 uk-navbar-container uk-dark uk-position-top uk-position-z-index" data-uk-navbar data-uk-scrollspy="cls:uk-animation-fade ">
+                <div className="uk-navbar-left uk-padding-small">
+                    <div className="uk-padding uk-padding-remove-vertical">
+                        <img className="logo" src='logo.png' alt="soulmountain" width="200px" />
+                    </div>
                 </div>
-            </div>
-            <div className="uk-navbar-right uk-padding uk-padding-remove-vertical">
-                <ul className="uk-navbar-nav">
-                    <div className='uk-padding-small'>
-                        <li className='uk-active'>
-                            <a className={styles.hvrPulseGrow} target="_blank" href="https://www.instagram.com/jordi_mestre_lopez/" >
-                                <img src='icons/instagram.png' width="30px" />
-                            </a>
-                        </li>
-                    </div>
-                    <div className='uk-padding-small'>
-                        <li>
-                            <a className={styles.hvrPulseGrow} target="_blank" href="https://www.youtube.com/channel/UCV3HknII0tr9Xgrm29cxYFA/featured" >
-                                <img src='icons/youtube.png' width="30px" />
-                            </a>
-                        </li>
-                    </div>
-                    <div className='uk-padding-small'>
-                        <li>
-                            <a className={styles.hvrPulseGrow} href="#contact" data-uk-scroll>
-                                <img src='icons/mail.png' width="30px" />
-                            </a>
-                        </li>
-                    </div>
-                </ul>
-            </div>
-        </nav>
+                <div className="uk-navbar-right uk-padding uk-padding-remove-vertical">
+                    <ul className="uk-navbar-nav">
+                        <div className='uk-padding-small'>
+                            <li className='uk-active'>
+                                <a className={styles.hvrPulseGrow} target="_blank" href="https://www.instagram.com/jordi_mestre_lopez/" >
+                                    <img src='icons/instagram.png' width="30px" />
+                                </a>
+                            </li>
+                        </div>
+                        <div className='uk-padding-small'>
+                            <li>
+                                <a className={styles.hvrPulseGrow} target="_blank" href="https://www.youtube.com/channel/UCV3HknII0tr9Xgrm29cxYFA/featured" >
+                                    <img src='icons/youtube.png' width="30px" />
+                                </a>
+                            </li>
+                        </div>
+                        <div className='uk-padding-small'>
+                            <li>
+                                <a className={styles.hvrPulseGrow} href="#contact" data-uk-scroll>
+                                    <img src='icons/mail.png' width="30px" />
+                                </a>
+                            </li>
+                        </div>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     </div>
 }
--- components/PressSection/index.js
+++ components/PressSection/index.js
@@ -7,12 +7,20 @@ export default function PressSection() {
     <div className="uk-padding uk-width-1-1" data-uk-scrollspy="cls:uk-animation-scale-up; delay:250; repeat: true">
       <h1 className="uk-padding-small uk-padding-remove-top uk-heading-line uk-text-center"><span>Press</span></h1>
     </div>
-    <div className="uk-padding uk-padding-remove-top uk-flex uk-flex-center">
+    <div className="uk-visible@s uk-padding uk-padding-remove-top uk-flex uk-flex-center">
       <div className="uk-padding uk-padding-remove-vertical" data-uk-scrollspy="cls:uk-animation-slide-bottom-medium; delay: 250; repeat: true">
         <Card description="La Mañana" href="documents\LaManyana.pdf" id="laManyana" src="pressImages\laManyanaImage.jpg" width="200px"/>
       </div>
       <div className="uk-padding uk-padding-remove-vertical" data-uk-scrollspy="cls:uk-animation-slide-top-medium; delay: 250; repeat: true">
         <Card description="Segre: Lectura" href="documents\SegreLectura.pdf#page=30" id="Segre" src="pressImages\segreImage.jpg" width="200px"/>
+      </div>
+    </div>
+    <div className="uk-hidden@s uk-padding uk-flex uk-flex-column">
+      <div className="uk-padding uk-padding-remove-bottom" data-uk-scrollspy="cls:uk-animation-slide-bottom-medium; delay: 250; repeat: true">
+        <Card description="La Mañana" href="documents\LaManyana.pdf" id="laManyana" src="pressImages\laManyanaImage.jpg" width="400px"/>
+      </div>
+      <div className="uk-padding uk-padding-remove-bottom" data-uk-scrollspy="cls:uk-animation-slide-top-medium; delay: 250; repeat: true">
+        <Card description="Lectura (diari Segre)" href="documents\SegreLectura.pdf#page=30" id="Segre" src="pressImages\segreImage.jpg" width="400px"/>
       </div>
     </div>
   </div>
