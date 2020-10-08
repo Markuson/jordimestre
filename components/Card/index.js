3db73d703684f72556d7b3b7e495bc9abdcb0468
Markuson <el.marcuson@gmail.com>
2020-10-08 11:47:32 +0200

    performance improvements

--- components/Card/index.js
+++ components/Card/index.js
@@ -9,8 +9,8 @@ export default function Card({
     return <div className="uk-card uk-card-default uk-box-shadow-hover-large" tabIndex="0">
             <a href={href} data-caption={description} target="blank" rel="noopener noreferrer">
                 <div id={id} className="uk-text-center uk-transition-toggle">
-                    <img width={width} src={src} />
-                    <div class="uk-transition-slide-bottom-small uk-overlay uk-overlay-primary uk-light uk-position-bottom">
+                    <img width={width} src={src} alt={description} />
+                    <div className="uk-transition-slide-bottom-small uk-overlay uk-overlay-primary uk-light uk-position-bottom">
                         <p>{description}</p>
                     </div>
                 </div>
