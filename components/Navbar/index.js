1dd9d6fc42328e0666b1f1663e915ad48af02bd7
Markuson <el.marcuson@gmail.com>
2020-10-08 11:55:37 +0200

    performance improvements

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -21,21 +21,21 @@ export default function Navbar() {
                         <li className='uk-active'>
                             <div className='uk-padding-small'>
                                 <a className={styles.hvrPulseGrow} target="_blank" href="https://www.instagram.com/jordi_mestre_lopez/" rel="noopener noreferrer">
-                                    <img src='icons/instagram.png' width="30px" height="30px" />
+                                    <img src='icons/instagram.png' alt="instagram" width="30px" height="30px" />
                                 </a>
                             </div>
                         </li>
                         <li>
                             <div className='uk-padding-small'>
                                 <a className={styles.hvrPulseGrow} target="_blank" href="https://www.youtube.com/channel/UCV3HknII0tr9Xgrm29cxYFA/featured" rel="noopener noreferrer">
-                                    <img src='icons/youtube.png' width="30px" height="30px" />
+                                    <img src='icons/youtube.png' alt="youtube" width="30px" height="30px" />
                                 </a>
                             </div>
                         </li>
                         <li>
                             <div className='uk-padding-small'>
                                 <a className={styles.hvrPulseGrow} href="#contact" rel="noopener noreferrer" data-uk-scroll>
-                                    <img src='icons/mail.png' width="30px" height="30px" />
+                                    <img src='icons/mail.png' alt="contact" width="30px" height="30px" />
                                 </a>
                             </div>
                         </li>
