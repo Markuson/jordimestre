3c11ba99b2d065e1e655ead4aabdf23be085ce2e
Markuson <el.marcuson@gmail.com>
2020-10-23 13:02:32 +0200

    resize pictures of the gallery

---
+++ components/Card/index.js
@@ -0,0 +1,19 @@
+export default function Card({
+    description,
+    href,
+    id,
+    src,
+    width = 200,
+}) {
+
+    return <div className="uk-card uk-card-default uk-box-shadow-hover-large" tabIndex="0">
+            <a href={href} data-caption={description} target="blank" rel="noopener noreferrer">
+                <div id={id} className="uk-text-center uk-transition-toggle">
+                    <img width={width} src={src} alt={description} />
+                    <div className="uk-transition-slide-bottom-small uk-overlay uk-overlay-primary uk-light uk-position-bottom">
+                        <p>{description}</p>
+                    </div>
+                </div>
+            </a>
+    </div>
+}
