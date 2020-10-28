46eca200a27e948db894eac27737a28b754db4cb
Markuson <el.marcuson@gmail.com>
2020-10-28 12:11:10 +0100

    changed contact seccion, aded recaptcha and directed to soulmountain.jordi@gmail.com

---
+++ utils/toastLiterals.js
@@ -0,0 +1,26 @@
+export default {
+    en: {
+        sendSuccess: `Email sent, we will reply you as soon as possible`,
+        sendError: `Sorry, an error has occurred. Try again or send an email to soulmountain.jordi@gmail.com`,
+        errorEmptyMail: `Email is empty`,
+        errorInvalidEmail: `Email not valid`,
+        errorEmptySubject: `Subject is empty`,
+        errorEmptyText:`Text is empty`,
+    },
+    es: {
+        sendSuccess: `Correo electrónico enviado, le responderemos lo antes posible`,
+        sendError: `Lo sentimos, se ha producido un error. Vuelve a intentarlo o envía un correo electrónico a soulmountain.jordi@gmail.com`,
+        errorEmptyMail: `El correo electrónico está vacío`,
+        errorInvalidEmail: `Correo electrónico no válido`,
+        errorEmptySubject: `El asunto está vacío`,
+        errorEmptyText:`El texto está vacío`,
+    },
+    ca: {
+        sendSuccess: `Correu electrònic enviat, us respondrem el més aviat possible`,
+        sendError: `Ho sentim, s'ha produït un error. Torneu-ho a provar o envieu un correu electrònic a soulmountain.jordi@gmail.com`,
+        errorEmptyMail: `El correu electrònic està buit`,
+        errorInvalidEmail: `Correu electrònic no vàlid`,
+        errorEmptySubject: `L’assumpte està buit`,
+        errorEmptyText:`El text està buit`,
+    }
+}
