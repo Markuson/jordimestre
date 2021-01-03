757100fbf6e5055041aeeea7c9972bbf777f5ed6
Markuson <el.marcuson@gmail.com>
2021-01-03 12:27:33 +0100

    added guitar.com link on press section

--- components/PressGallery/index.js
+++ components/PressGallery/index.js
@@ -5,29 +5,33 @@ import PressImage from '../PressImage'
 const handleDragStart = (e) => e.preventDefault();
 
 const items = [
-    <PressImage onDragStart={handleDragStart} description="La Mañana" href="documents\LaManyana.pdf" id="laManyana" src="pressImages\lamanana.png" width="280px" height="280px" />,
-    <PressImage onDragStart={handleDragStart} description="Lectura (diari Segre)" href="documents\SegreLectura.jpg" id="Segre" src="pressImages\segre.png" width="280px" height="280px" />,
-    <PressImage onDragStart={handleDragStart} description="La Sexta" href="videos\sexta.mp4 " id="Sexta" src="pressImages\lasexta.png" width="280px" height="280px" />,
-    <PressImage onDragStart={handleDragStart} description="Televisió de Catalunya" href="videos\tv3.mp4" id="tv3" src="pressImages\tv3.png" width="280px" height="280px" />,
-    <PressImage onDragStart={handleDragStart} description="Catalunya Radio" href="https://www.google.com/amp/s/www.ccma.cat/catradio/alacarta/eth-maitin-daran/jordi-mestre-encete-eth-projecte-musicau-soul-mountain-enregistrant-a-3-010-metres-de-nautada/audio-amp/1077636/" id="CatRadio" src="pressImages\catalunyaRadio.png" width="280px" height="280px" />,
-    <PressImage onDragStart={handleDragStart} description="Enderrock" href="http://www.enderrock.cat/noticies/etiqueta/Jordi+Mestre" id="Enderrock" src="pressImages\enderrock.png" width="280px" height="280px" />,
-    <PressImage onDragStart={handleDragStart} description="Territoris.cat" href="https://www.territoris.cat/articulo/la-panoramica/jordi-mestre/20200807093609066749.html" id="TerritorisCat" src="pressImages\territoris.png" width="280px" height="280px" />,
+    <PressImage onDragStart={handleDragStart} description="Monocreators blog" href="https://blog.monocreators.com/soul-mountain-music-at-the-mountain-peak/" id="Monoblog" src="pressImages\mono.png" width="280px" height="280px" />,
+    <PressImage onDragStart={handleDragStart} description="Guitar Magazine" href="https://guitar.com/features/interviews/meet-the-man-who-scaled-a-3000m-mountain-with-his-guitar-just-to-record-a-song/" id="guitarmagazine" src="pressImages\guitardotcom.png" width="280px" height="280px" />,
     <PressImage onDragStart={handleDragStart} description="Nacio Muntanya" href="https://www.naciodigital.cat/naciomuntanya/noticia/6336/jordi-mestre-convertim-cim-mes-3000-metres-estudi-gravacio" id="NacioMuntanya" src="pressImages\nacioMuntanya.png" width="280px" height="280px" />,
     <PressImage onDragStart={handleDragStart} description="Federacio de entitats escursionistes" href="https://www.feec.cat/actualitat/noticies/musica-i-muntanya-es-fusionen-a-soul-mountain/" id="Feec" src="pressImages\feec.png" width="280px" height="280px" />,
-    <PressImage onDragStart={handleDragStart} description="Monocreators blog" href="https://blog.monocreators.com/soul-mountain-music-at-the-mountain-peak/" id="Monoblog" src="pressImages\mono.png" width="280px" height="280px" />,
+    <PressImage onDragStart={handleDragStart} description="Enderrock" href="http://www.enderrock.cat/noticies/etiqueta/Jordi+Mestre" id="Enderrock" src="pressImages\enderrock.png" width="280px" height="280px" />,
+    <PressImage onDragStart={handleDragStart} description="Televisió de Catalunya" href="videos\tv3.mp4" id="tv3" src="pressImages\tv3.png" width="280px" height="280px" />,
+    <PressImage onDragStart={handleDragStart} description="La Sexta" href="videos\sexta.mp4 " id="Sexta" src="pressImages\lasexta.png" width="280px" height="280px" />,
+    <PressImage onDragStart={handleDragStart} description="Catalunya Radio" href="https://www.google.com/amp/s/www.ccma.cat/catradio/alacarta/eth-maitin-daran/jordi-mestre-encete-eth-projecte-musicau-soul-mountain-enregistrant-a-3-010-metres-de-nautada/audio-amp/1077636/" id="CatRadio" src="pressImages\catalunyaRadio.png" width="280px" height="280px" />,
+    <PressImage onDragStart={handleDragStart} description="La Mañana" href="documents\LaManyana.pdf" id="laManyana" src="pressImages\lamanana.png" width="280px" height="280px" />,
+    <PressImage onDragStart={handleDragStart} description="Lectura (diari Segre)" href="documents\SegreLectura.jpg" id="Segre" src="pressImages\segre.png" width="280px" height="280px" />,
+    <PressImage onDragStart={handleDragStart} description="Territoris.cat" href="https://www.territoris.cat/articulo/la-panoramica/jordi-mestre/20200807093609066749.html" id="TerritorisCat" src="pressImages\territoris.png" width="280px" height="280px" />,
+
 ];
 
 const itemsMobile = [
-    <PressImage onDragStart={handleDragStart} description="La Mañana" href="documents\LaManyana.pdf" id="laManyana" src="pressImages\lamanana.png" width="135px" height="135px" />,
-    <PressImage onDragStart={handleDragStart} description="Lectura (diari Segre)" href="documents\SegreLectura.jpg" id="Segre" src="pressImages\segre.png" width="135px" height="135px" />,
-    <PressImage onDragStart={handleDragStart} description="La Sexta" href="videos\sexta.mp4" id="Sexta" src="pressImages\lasexta.png" width="135px" height="135px" />,
-    <PressImage onDragStart={handleDragStart} description="Televisió de Catalunya" href="videos\tv3.mp4" id="Segre" src="pressImages\tv3.png" width="135px" height="135px" />,
-    <PressImage onDragStart={handleDragStart} description="Catalunya Radio" href="https://www.google.com/amp/s/www.ccma.cat/catradio/alacarta/eth-maitin-daran/jordi-mestre-encete-eth-projecte-musicau-soul-mountain-enregistrant-a-3-010-metres-de-nautada/audio-amp/1077636/" id="CatRadio" src="pressImages\catalunyaRadio.png" width="135px" height="135px" />,
-    <PressImage onDragStart={handleDragStart} description="Enderrock" href="http://www.enderrock.cat/noticies/etiqueta/Jordi+Mestre" id="Enderrock" src="pressImages\enderrock.png" width="135px" height="135px" />,
-    <PressImage onDragStart={handleDragStart} description="Territoris.cat" href="https://www.territoris.cat/articulo/la-panoramica/jordi-mestre/20200807093609066749.html" id="TerritorisCat" src="pressImages\territoris.png" width="135px" height="135px" />,
+    <PressImage onDragStart={handleDragStart} description="Monocreators blog" href="https://blog.monocreators.com/soul-mountain-music-at-the-mountain-peak/" id="Monoblog" src="pressImages\mono.png" width="135px" height="135px" />,
+    <PressImage onDragStart={handleDragStart} description="Guitar Magazine" href="https://guitar.com/features/interviews/meet-the-man-who-scaled-a-3000m-mountain-with-his-guitar-just-to-record-a-song/" id="guitarmagazine" src="pressImages\guitardotcom.png" width="135px" height="135px" />,
     <PressImage onDragStart={handleDragStart} description="Nacio Muntanya" href="https://www.naciodigital.cat/naciomuntanya/noticia/6336/jordi-mestre-convertim-cim-mes-3000-metres-estudi-gravacio" id="NacioMuntanya" src="pressImages\nacioMuntanya.png" width="135px" height="135px" />,
     <PressImage onDragStart={handleDragStart} description="Federacio de entitats escursionistes" href="https://www.feec.cat/actualitat/noticies/musica-i-muntanya-es-fusionen-a-soul-mountain/" id="Feec" src="pressImages\feec.png" width="135px" height="135px" />,
-    <PressImage onDragStart={handleDragStart} description="Monocreators blog" href="https://blog.monocreators.com/soul-mountain-music-at-the-mountain-peak/" id="Monoblog" src="pressImages\mono.png" width="135px" height="135px" />,
+    <PressImage onDragStart={handleDragStart} description="Enderrock" href="http://www.enderrock.cat/noticies/etiqueta/Jordi+Mestre" id="Enderrock" src="pressImages\enderrock.png" width="135px" height="135px" />,
+    <PressImage onDragStart={handleDragStart} description="Televisió de Catalunya" href="videos\tv3.mp4" id="Segre" src="pressImages\tv3.png" width="135px" height="135px" />,
+    <PressImage onDragStart={handleDragStart} description="La Sexta" href="videos\sexta.mp4" id="Sexta" src="pressImages\lasexta.png" width="135px" height="135px" />,
+    <PressImage onDragStart={handleDragStart} description="Catalunya Radio" href="https://www.google.com/amp/s/www.ccma.cat/catradio/alacarta/eth-maitin-daran/jordi-mestre-encete-eth-projecte-musicau-soul-mountain-enregistrant-a-3-010-metres-de-nautada/audio-amp/1077636/" id="CatRadio" src="pressImages\catalunyaRadio.png" width="135px" height="135px" />,
+    <PressImage onDragStart={handleDragStart} description="La Mañana" href="documents\LaManyana.pdf" id="laManyana" src="pressImages\lamanana.png" width="135px" height="135px" />,
+    <PressImage onDragStart={handleDragStart} description="Lectura (diari Segre)" href="documents\SegreLectura.jpg" id="Segre" src="pressImages\segre.png" width="135px" height="135px" />,
+    <PressImage onDragStart={handleDragStart} description="Territoris.cat" href="https://www.territoris.cat/articulo/la-panoramica/jordi-mestre/20200807093609066749.html" id="TerritorisCat" src="pressImages\territoris.png" width="135px" height="135px" />,
+
 ];
 
 export default function PressGallery() {
